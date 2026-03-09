1cf32b35766364352d1de64e74d335944429c6c8
ginkageshigakure-droid <ginkage.shigakure@gmail.com>
2026-03-09 20:40:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function login() {
+    const u = document.getElementById("user").value;
+    const p = document.getElementById("pass").value;
+
+    // EXTREEM STERK WACHTWOORD
+    const superWachtwoord = "X9!pA7#qT2vL@8rZ$wF4mN%kH6sB*3dJ0yR";
+
+    if (u === "admin" && p === superWachtwoord) {
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("msg").textContent = "Fout wachtwoord";
+    }
+}
 
